c4be994df89340203628fe5ed5445edf919860d9
pogsandro25 <pogsandro@gmail.com>
2024-07-12 15:48:05 +0700

    fix warnings

--- src/Lexer.java
+++ src/Lexer.java
@@ -1,22 +1,17 @@
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-
 public class Lexer {
-
     public static List<Token> tokenize(String input) {
-        List<Token> tokens = new ArrayList<Token>();
+        List<Token> tokens = new ArrayList<>();
         String[] splitByLine = input.split("\n");
-
-        List<String> splitSpcLine = new ArrayList<String>();
+        List<String> splitSpcLine = new ArrayList<>();
         for (String s : splitByLine) {
             String[] splitBySpace = s.split(" ");
             splitSpcLine.addAll(Arrays.asList(splitBySpace));
         }
-
         for (String elsSplitSpcLine : splitSpcLine) {
             boolean isStartsWithSpecialSymbol = (elsSplitSpcLine.charAt(0) == '#' || elsSplitSpcLine.charAt(0) == '@');
-
             if (isStartsWithSpecialSymbol && elsSplitSpcLine.charAt(elsSplitSpcLine.length() - 1) == ';') {
                 tokens.add(new Token(String.valueOf(elsSplitSpcLine.charAt(0))));
                 tokens.add(new Token(elsSplitSpcLine.substring(1, elsSplitSpcLine.length() - 1)));
